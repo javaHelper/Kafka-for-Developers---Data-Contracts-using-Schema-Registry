06d5c18b015993e84de392972e16a3169a91af96
Prateek <54174687+javaHelper@users.noreply.github.com>
2023-04-17 18:48:50 +0530

    Add a timestamp, decimal logical type
    26. Add a timestamp, decimal logical type to the CoffeeOrder Schema

---
+++ 04-avro-coffee-shop-logical-schema-types-date-long/coffee-orders-service/src/main/java/com/learnavro/util/CoffeeOrderUtil.java
@@ -0,0 +1,57 @@
+package com.learnavro.util;
+
+import com.github.javafaker.Faker;
+import com.learnavro.domain.generated.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.Random;
+
+public class CoffeeOrderUtil {
+    private static final Faker FAKER = Faker.instance();
+
+    public static CoffeeOrder buildNewCoffeeOrder() {
+        return CoffeeOrder.newBuilder()
+                .setId(randomId())
+                .setName(FAKER.name().fullName())
+                .setStore(generateStore())
+                .setOrderLineItems(generateOrderLineItems())
+                .setOrderedTime(Instant.now())
+                .setStatus("NEW")
+                .build();
+    }
+
+
+    private static List<OrderLineItem> generateOrderLineItems() {
+        var orderLineItem = OrderLineItem.newBuilder()
+                .setName("Caffe Latte")
+                .setQuantity(1)
+                .setSize(Size.MEDIUM)
+                .setCost(BigDecimal.valueOf(3.99))
+                .build();
+
+        return List.of(orderLineItem);
+    }
+
+    private static Store generateStore() {
+        return Store.newBuilder()
+                .setId(randomId())
+                .setAddress(buildAddress())
+                .build();
+    }
+
+    private static Address buildAddress() {
+        return Address.newBuilder()
+                .setAddressLine1(FAKER.address().fullAddress())
+                .setCity(FAKER.address().city())
+                .setStateProvince(FAKER.address().state())
+                .setZip(FAKER.address().zipCode())
+                .build();
+    }
+
+    public static int randomId() {
+        Random random = new Random();
+        return random.nextInt(1000);
+    }
+}
