3174cd3af398e9725ca08b98f26d43f0c0be8c05
Prateek <54174687+javaHelper@users.noreply.github.com>
2023-04-18 19:03:00 +0530

    Publish and Consumer Record using Schema Registry

---
+++ 07-schema-registry-publish-and-consumer/coffee-orders-consumer/src/main/java/com/learnavro/consumer/CoffeeOrderConsumerSchemaRegistry.java
@@ -0,0 +1,56 @@
+package com.learnavro.consumer;
+
+import com.learnavro.domain.generated.CoffeeOrder;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.Collections;
+import java.util.Properties;
+
+public class CoffeeOrderConsumerSchemaRegistry {
+    private static final Logger log = LoggerFactory.getLogger(CoffeeOrderConsumerSchemaRegistry.class);
+    private static final String COFFEE_ORDERS = "coffee-orders-sr";
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "coffee.consumer.sr");
+        properties.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+        properties.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+
+        KafkaConsumer<String, CoffeeOrder> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList(COFFEE_ORDERS));
+        log.info("Consumer Started");
+
+        while(true){
+            ConsumerRecords<String,CoffeeOrder> records  = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, CoffeeOrder> record : records){
+                try{
+                    log.info("Consumed Message , key : {} , value : {}", record.key(), record.value().toString());
+                    var coffeeOrder = record.value();
+
+                    //ZoneId.SHORT_IDS
+                    var utcDateTime = LocalDateTime.ofInstant(coffeeOrder.getOrderedTime(), ZoneOffset.UTC);
+                    var cstDateTime = LocalDateTime.ofInstant(coffeeOrder.getOrderedTime(), ZoneId.of("America/Chicago"));
+                    log.info("utcDateTime: {} , cstDateTime : {} ",utcDateTime,  cstDateTime);
+
+                }catch (Exception e){
+                    log.error("Exception is : {} ", e.getMessage(), e);
+                }
+            }
+        }
+    }
+}
---
+++ 07-schema-registry-publish-and-consumer/coffee-orders-service/src/main/java/com/learnavro/producer/CoffeeOrderProducerSchemaRegistry.java
@@ -0,0 +1,40 @@
+package com.learnavro.producer;
+
+import com.learnavro.domain.generated.CoffeeOrder;
+import com.learnavro.util.CoffeeOrderUtil;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class CoffeeOrderProducerSchemaRegistry {
+    private static final Logger log = LoggerFactory.getLogger(CoffeeOrderProducerSchemaRegistry.class);
+    private static final String COFFEE_ORDERS = "coffee-orders-sr";
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+
+        // If we don't use below, then exception will be thrown, to make this working, please use Generic Record instead
+        // of CoffeeOrder
+        properties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+
+        KafkaProducer<String, CoffeeOrder> producer = new KafkaProducer<>(properties);
+
+        CoffeeOrder coffeeOrder = CoffeeOrderUtil.buildNewCoffeeOrder();
+
+        ProducerRecord<String, CoffeeOrder> producerRecord =
+                new ProducerRecord<>(COFFEE_ORDERS, coffeeOrder);
+
+        var recordMetaData = producer.send(producerRecord).get();
+        log.info("recordMetaData : {}", recordMetaData);
+    }
+}
